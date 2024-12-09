117031b4991e13b3326d438bf9aa912d31b50b8a
Jaehwan Hyun <jec0101@naver.com>
2024-12-09 15:09:19 +0900

    fix: API 서버로 연결

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,7 @@
 // const String baseUrl = 'http://localhost:8080';
 // const String websocketUrl = 'ws://localhost:8080/connection';
 
-const String BASE_URL = 'http://10.0.2.2:8080';
-const String WEBSOCKET_URL = 'ws://10.0.2.2:8080';
-
+// const String BASE_URL = 'http://10.0.2.2:8080';
+// const String WEBSOCKET_URL = 'ws://10.0.2.2:8080';
+const String BASE_URL = 'http://3.35.175.114:8080';
+const String WEBSOCKET_URL = 'ws://3.35.175.114:8080';
